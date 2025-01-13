60802917d7a3ff5c9aa2c9d451f48f5828a9be53
lengmuzhaxi <2690497440@qq.com>
2025-01-13 14:14:51 +0800

    exercises 11

--- exercises/11_method/main.cpp
+++ exercises/11_method/main.cpp
@@ -1,14 +1,20 @@
 #include "../exercise.h"
 
 struct Fibonacci {
-    unsigned long long cache[128];
-    int cached;
+    unsigned long long cache[128]={0};
+    int cached=2;
 
     // TODO: 实现正确的缓存优化斐波那契计算
     unsigned long long get(int i) {
-        for (; false; ++cached) {
-            cache[cached] = cache[cached - 1] + cache[cached - 2];
+       if (i < cached) {  // 如果缓存中已有此值，直接返回
+            return cache[i];
         }
+
+        // 否则，计算并更新缓存
+        for (int j = cached; j <= i; ++j) {
+            cache[j] = cache[j - 1] + cache[j - 2];
+        }
+        cached = i + 1;  // 更新已缓存的最大索引
         return cache[i];
     }
 };
@@ -16,6 +22,8 @@ struct Fibonacci {
 int main(int argc, char **argv) {
     // TODO: 初始化缓存结构体，使计算正确
     Fibonacci fib;
+     fib.cache[0] = 0;  // 确保cache[0]是0
+    fib.cache[1] = 1;
     ASSERT(fib.get(10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << fib.get(10) << std::endl;
     return 0;
