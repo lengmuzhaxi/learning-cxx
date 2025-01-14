8dbb69b698a3e091a067dbb13857a5a9733c81a8
lengmuzhaxi <2690497440@qq.com>
2025-01-14 11:11:55 +0800

    exercises 14

--- exercises/14_class_destruct/main.cpp
+++ exercises/14_class_destruct/main.cpp
@@ -11,17 +11,30 @@ class DynFibonacci {
 
 public:
     // TODO: 实现动态设置容量的构造器
-    DynFibonacci(int capacity): cache(new ?), cached(?) {}
+    DynFibonacci(int capacity): cache(new size_t[capacity]), cached(1) {
+        cache[0] = 0;  // 初始化 Fibonacci(0)
+        cache[1] = 1;  // 初始化 Fibonacci(1)
+        for (int i = 2; i < capacity; ++i) {
+            cache[i] = 0;  // 未初始化部分置为 0
+        }
+    }
 
     // TODO: 实现析构器，释放缓存空间
-    ~DynFibonacci();
+    ~DynFibonacci()
+    {
+         delete[] cache;
+    }
 
     // TODO: 实现正确的缓存优化斐波那契计算
     size_t get(int i) {
-        for (; false; ++cached) {
-            cache[cached] = cache[cached - 1] + cache[cached - 2];
+        // 如果缓存未命中，则计算并存储
+        for (int j = 2; j <= i; ++j) {
+            if (cache[j] == 0) {  // 仅当值未被缓存时计算
+                cache[j] = cache[j - 1] + cache[j - 2];
+            }
         }
-        return cache[i];
+
+        return cache[i];  // 返回缓存值
     }
 };
 
